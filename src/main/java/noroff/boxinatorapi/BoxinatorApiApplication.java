6c5c17d86115d6f2da6bb81bbcc659df2494fcb7
Richie Schuurman <richie.schuurman@hotmail.com>
2022-04-04 14:36:54 +0200

    init commit

---
+++ src/main/java/noroff/boxinatorapi/BoxinatorApiApplication.java
@@ -0,0 +1,13 @@
+package noroff.boxinatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxinatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxinatorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/noroff/boxinatorapi/BoxinatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package noroff.boxinatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxinatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
