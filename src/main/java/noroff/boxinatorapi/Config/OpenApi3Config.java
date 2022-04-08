c6425b69770659b81a414f85ee69e5ad1ddf2c3e
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-04-08 18:20:04 +0200

    Correct token URL

--- src/main/java/noroff/boxinatorapi/Config/OpenApi3Config.java
+++ src/main/java/noroff/boxinatorapi/Config/OpenApi3Config.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @SecurityScheme(name = "keycloak_implicit", type = SecuritySchemeType.OAUTH2,
         flows = @OAuthFlows(authorizationCode = @OAuthFlow(
                 authorizationUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/auth",
-                tokenUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/token}",
+                tokenUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/token",
                 scopes = {
                         @OAuthScope(name = "openid", description = "OpenID Connect Endpoints"),
                         @OAuthScope(name = "profile", description = "Standard profile claims excluding phone and email"),
