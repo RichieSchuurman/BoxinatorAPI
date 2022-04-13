a517886b95d46a1a772b79f0c7c5e1e88dc3cc0f
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-04-13 22:10:26 +0200

    Enable fetching user accounts by keycloak subject id

--- src/main/java/noroff/boxinatorapi/Controllers/AccountController.java
+++ src/main/java/noroff/boxinatorapi/Controllers/AccountController.java
@@ -1,4 +1,45 @@
 package noroff.boxinatorapi.Controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import noroff.boxinatorapi.Models.Account;
+import noroff.boxinatorapi.Models.CommonResponse;
+import noroff.boxinatorapi.Services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@SecurityRequirement(name = "keycloak_implicit")
+@RequestMapping("api/account")
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Operation(summary = "Get the account information of a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found account information of specific user",
+                        content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Account.class))}),
+            @ApiResponse(responseCode = "404", description = "Account information not found",
+                        content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = CommonResponse.class))})
+    })
+    @GetMapping("/{keycloakSubjectId}")
+    public ResponseEntity<CommonResponse> getAccountByKeycloakSubjectId(
+            HttpServletRequest request,
+            @Parameter(description = "Keycloak subject id of the account to be fetched") @PathVariable String keycloakSubjectId) {
+        return accountService.getAccountByKeycloakSubjectId(request, keycloakSubjectId);
+    }
 }
--- src/main/java/noroff/boxinatorapi/Services/AccountService.java
+++ src/main/java/noroff/boxinatorapi/Services/AccountService.java
@@ -1,4 +1,87 @@
 package noroff.boxinatorapi.Services;
 
+import noroff.boxinatorapi.Models.Account;
+import noroff.boxinatorapi.Models.CommonResponse;
+import noroff.boxinatorapi.Repositories.AccountRepository;
+import noroff.boxinatorapi.Utilities.Command;
+import noroff.boxinatorapi.Utilities.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account getAccountByJwt(Jwt principal) {
+        // TODO: Implement account type
+        Account account = new Account();
+        String email = principal.getClaimAsString("email");
+        String keyCloakSubjectId = principal.getClaimAsString("sub");
+        String firstName = principal.getClaimAsString("given_name");
+        String lastName = principal.getClaimAsString("family_name");
+        String dateOfBirth = principal.getClaimAsString("dob");
+        String countryOfResidence = principal.getClaimAsString("country_of_residence");
+        String postalCode = principal.getClaimAsString("postal_code");
+        String contactNumber = principal.getClaimAsString("contact_number");
+
+        if (accountRepository.existsAccountByEmail(email)) {
+            Optional<Account> accountRepos = accountRepository.findAccountByEmail(email);
+            account = accountRepos.get();
+        } else {
+            // Account does not yet exist: set attributes of the new account
+            account.setEmail(email);
+            account.setKeycloakSubjectId(keyCloakSubjectId);
+            account.setFirstName(firstName);
+            account.setLastName(lastName);
+
+            // Optional account attributes
+            if (dateOfBirth != null) {
+                account.setDateOfBirth(dateOfBirth);
+            }
+            if (countryOfResidence != null) {
+                account.setCountryOfResidence(countryOfResidence);
+            }
+            if (postalCode != null) {
+                account.setPostalCode(postalCode);
+            }
+            if (contactNumber != null) {
+                account.setContactNumber(contactNumber);
+            }
+
+            account = accountRepository.save(account);
+        }
+
+        return account;
+    }
+
+    public ResponseEntity<CommonResponse> getAccountByKeycloakSubjectId(HttpServletRequest request, String keyCloakSubjectId) {
+        Account account;
+        Command cmd = new Command(request);
+        CommonResponse commonResponse = new CommonResponse();
+        HttpStatus resp;
+
+        if (accountRepository.existsAccountByKeycloakSubjectId(keyCloakSubjectId)) {
+            Optional<Account> accountRepos = accountRepository.findAccountByKeycloakSubjectId(keyCloakSubjectId);
+            account = accountRepos.get();
+            commonResponse.data = account;
+            commonResponse.message = "Found account with id: " + keyCloakSubjectId;
+            resp = HttpStatus.OK;
+        } else {
+            commonResponse.data = null;
+            commonResponse.message = "Account not found";
+            resp = HttpStatus.NOT_FOUND;
+        }
+
+        cmd.setResult(resp);
+        Logger.getInstance().logCommand(cmd);
+        return new ResponseEntity<>(commonResponse, resp);
+    }
 }
