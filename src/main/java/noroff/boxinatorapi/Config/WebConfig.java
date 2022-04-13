0fe7c5ab42a603792cfb0f0103d31bdb588d69e0
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-04-13 22:06:19 +0200

    Create and configure account interceptor

---
+++ src/main/java/noroff/boxinatorapi/Config/WebConfig.java
@@ -0,0 +1,21 @@
+package noroff.boxinatorapi.Config;
+
+import noroff.boxinatorapi.Utilities.AccountInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    AccountInterceptor accountInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(accountInterceptor);
+    }
+}
