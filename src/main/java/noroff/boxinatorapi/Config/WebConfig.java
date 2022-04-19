edd818ffb2d5b9893dfd92ba76ddb56b8442f2e2
Richie Schuurman <richie.schuurman@hotmail.com>
2022-04-19 10:59:45 +0200

    readded enum converter

--- src/main/java/noroff/boxinatorapi/Config/WebConfig.java
+++ src/main/java/noroff/boxinatorapi/Config/WebConfig.java
@@ -3,6 +3,7 @@ package noroff.boxinatorapi.Config;
 import noroff.boxinatorapi.Utilities.AccountInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(accountInterceptor);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToEnumConverter());
     }
 }
